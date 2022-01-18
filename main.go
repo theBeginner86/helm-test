606c74cde7d1e30d3d61d05e5488398b6cada025
Pranav Singh <pranavsingh02@hotmail.com>
2022-01-18 16:09:16 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"github.com/pkg/browser"
+)
+
+func main(){
+	fmt.Printf("Hello World")
+	var url string
+	fmt.Printf("Enter your URL: ")
+	fmt.Scan(&url)
+
+	fmt.Printf("Opening %s ...\n", url)
+
+	err := browser.OpenURL(url)
+	if err != nil {
+		fmt.Printf("Error occured")
+		fmt.Print(err)
+	} else {
+		fmt.Printf("Successfully opened %s in the browsergo.", url)
+	}
+
+}
